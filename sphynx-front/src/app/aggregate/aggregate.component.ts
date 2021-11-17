5c809cff7aebbb73a650af5f9d5f9ac7c135a18b
Gauthier Dubourdieu <gdubourd@gmail.com>
2021-11-17 16:11:52 +0100

    Exercice technique - Front - Ajout filtrage contenu tableau

--- sphynx-front/src/app/aggregate/aggregate.component.ts
+++ sphynx-front/src/app/aggregate/aggregate.component.ts
@@ -3,6 +3,7 @@ import {WebsocketService} from "../services/ws/websocket.service";
 import {environment} from "../../environments/environment";
 import {HttpClient} from "@angular/common/http";
 import {AggregateDto} from "../model/AggregateDto";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-aggregate',
@@ -13,8 +14,14 @@ export class AggregateComponent implements OnInit {
 
   aggregates: AggregateDto[] = [];
   displayedColumns: string[] = ['country', 'value'];
+  startDate: Date;
+  endDate: Date;
 
-  constructor(private wsService: WebsocketService, private http: HttpClient) {
+  constructor(private wsService: WebsocketService, private http: HttpClient, private route: ActivatedRoute) {
+    this.route.queryParams.subscribe(params => {
+      this.startDate = params['dateDebut'];
+      this.endDate = params['dateFin'];
+    });
   }
 
   ngOnInit() {
@@ -26,10 +33,27 @@ export class AggregateComponent implements OnInit {
   }
 
   private loadData() {
-    this.http.get(environment.serverUrl + "/aggregate")
-      .subscribe((data: AggregateDto[]) => {
-        this.aggregates = data;
-      });
+    if((this.startDate != undefined) && (this.endDate != undefined)) {
+      let startDateIso = new Date(this.startDate);
+      let endDateIso = new Date(this.endDate);
+
+      this.http.get(environment.serverUrl + "/aggregateBetweenDates",
+        {
+          params: {
+            from: startDateIso.toISOString(),
+            to: endDateIso.toISOString()
+          }
+        }) 
+        .subscribe((data: AggregateDto[]) => {
+          this.aggregates = data;
+        });
+    }
+    else {
+      this.http.get(environment.serverUrl + "/aggregate")
+        .subscribe((data: AggregateDto[]) => {
+          this.aggregates = data;
+        });
+    }
   }
 
 }
